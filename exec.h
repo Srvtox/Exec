f824db815315af1701479ad3048609c0536d6953
Srvtox <mr.nobody.cxyz@gmail.com>
2025-09-18 17:04:55 +0330

    Update exec.h

--- exec.h
+++ exec.h
@@ -1,19 +1,21 @@
-
 #ifndef _JACKPAL_PROCESS_H
 #define _JACKPAL_PROCESS_H 1
 
 #include <stddef.h>
-#include "jni.h"
+#include <jni.h>
 #include <android/log.h>
 
 #define LOG_TAG "jackpal-termexec"
 
+#ifdef __cplusplus
 extern "C" {
-JNIEXPORT jint JNICALL Java_jackpal_androidterm_TermExec_createSubprocessInternal
-      (JNIEnv *, jclass, jstring, jobjectArray, jobjectArray, jint);
+#endif
 
-    JNIEXPORT jint JNICALL Java_jackpal_androidterm_TermExec_waitFor
-      (JNIEnv *, jclass, jint);
+JNIEXPORT jint JNICALL Java_com_example_clangrunner_NativeExec_run
+  (JNIEnv *, jclass, jstring, jobjectArray, jobjectArray, jstring);
+
+#ifdef __cplusplus
 }
+#endif
 
-#endif        /* !defined(_JACKPAL_PROCESS_H) */
+#endif /* _JACKPAL_PROCESS_H */
